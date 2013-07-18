177522a00e6167b733c503f42a9d32f78d1dccb6
Vitalii Savchuk <esvit666@gmail.com>
2013-07-19 00:14:08 +0300

    init commit

---
+++ src/scripts/bzSwitch.js
@@ -0,0 +1,29 @@
+define('directives/bzSwitcher', ['main', 'bootstrap-switch'], function(module) {
+
+    module.directive('bzSwitcher', ['$parse', function($parse) {
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            link: function(scope, element, attrs, ngModel) {
+                var callback = $parse(attrs.bzSwitcher);
+
+                var onChange = function() {
+                    ngModel.$setViewValue(checkbox.bootstrapSwitch('status'));
+                    callback(scope);
+                    if (!scope.$$phase) {
+                        scope.$apply();
+                    }
+                };
+                var checkbox = $(element).wrap('<div class="switch"></div>').parent().bootstrapSwitch();
+                ngModel.$render = function(value) {
+                    checkbox.unbind('change');
+                    checkbox.bootstrapSwitch('setState', value);
+                    checkbox.change(onChange);
+                }
+                scope.$watch(attrs.ngModel, ngModel.$render);
+                checkbox.change(onChange);
+            }
+        };
+    }]);
+
+});
